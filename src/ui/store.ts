a645da47e4bdced362969463e2fea4ead5b576dd
qawatake <greenmackey@gmail.com>
2022-02-17 10:23:58 +0000

    remove redundant codes

--- src/ui/store.ts
+++ src/ui/store.ts
@@ -1,8 +1,6 @@
-import type { Switcher } from 'components/Switcher';
 import type CardViewSwitcherPlugin from 'main';
 import type { App } from 'obsidian';
 import { writable } from 'svelte/store';
 
 export const app = writable<App>();
 export const plugin = writable<CardViewSwitcherPlugin>();
-export const switcherComponent = writable<Switcher>();
